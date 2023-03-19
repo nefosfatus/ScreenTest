16e754c464167a0a682248f0c524926b749f719f
Ilyas <ilyas.balgabekov@hardcode.kz>
2023-03-19 18:05:50 +0600

    Add automapper and default profile for it

---
+++ Company.Delivery.Infrastructure/Mapping/DefaultMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Company.Delivery.Core;
+using Company.Delivery.Domain.Dto;
+
+namespace Company.Delivery.Infrastructure.Mapping;
+
+public class DefaultMapper : Profile
+{
+    public DefaultMapper()
+    {
+        CreateMap<Waybill, WaybillDto>();
+        CreateMap<WaybillCreateDto, Waybill>();
+        CreateMap<WaybillUpdateDto, Waybill>();
+    }
+}

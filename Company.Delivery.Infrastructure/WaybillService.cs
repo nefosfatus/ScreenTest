85957e55a678fbb0f6009535102fbc0c4d3f4d3f
Ilyas <ilyas.balgabekov@hardcode.kz>
2023-03-19 18:06:40 +0600

    Implement waybill service

--- Company.Delivery.Infrastructure/WaybillService.cs
+++ Company.Delivery.Infrastructure/WaybillService.cs
@@ -1,33 +1,60 @@
-﻿using Company.Delivery.Domain;
+﻿using AutoMapper;
+using Company.Delivery.Core;
+using Company.Delivery.Database;
+using Company.Delivery.Domain;
 using Company.Delivery.Domain.Dto;
+using Microsoft.EntityFrameworkCore;
 
 namespace Company.Delivery.Infrastructure;
 
 public class WaybillService : IWaybillService
 {
-    public Task<WaybillDto> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    private readonly DeliveryDbContext _context;
+    private readonly IMapper _mapper;
+    public WaybillService(DeliveryDbContext context, IMapper mapper)
     {
-        // TODO: Если сущность не найдена по идентификатору, кинуть исключение типа EntityNotFoundException
-
-        throw new NotImplementedException();
+        _context = context;
+        _mapper = mapper;
     }
 
-    public Task<WaybillDto> CreateAsync(WaybillCreateDto data, CancellationToken cancellationToken)
+    public async Task<WaybillDto> GetByIdAsync(Guid id, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var waybill = await _context.Waybills.AsNoTracking().FirstOrDefaultAsync(s=>s.Id == id, cancellationToken);
+        if (waybill is null)
+            throw new EntityNotFoundException();
+
+        return _mapper.Map<WaybillDto>(waybill);
     }
 
-    public Task<WaybillDto> UpdateByIdAsync(Guid id, WaybillUpdateDto data, CancellationToken cancellationToken)
+    public async Task<WaybillDto> CreateAsync(WaybillCreateDto data, CancellationToken cancellationToken)
     {
-        // TODO: Если сущность не найдена по идентификатору, кинуть исключение типа EntityNotFoundException
+        var toCreate = _mapper.Map<Waybill>(data);
+        var waybill = await _context.Waybills.AddAsync(toCreate, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
 
-        throw new NotImplementedException();
+        return _mapper.Map<WaybillDto>(waybill);
     }
 
-    public Task DeleteByIdAsync(Guid id, CancellationToken cancellationToken)
+    public async Task<WaybillDto> UpdateByIdAsync(Guid id, WaybillUpdateDto data, CancellationToken cancellationToken)
     {
-        // TODO: Если сущность не найдена по идентификатору, кинуть исключение типа EntityNotFoundException
+        var waybill = await _context.Waybills.FirstOrDefaultAsync(s => s.Id == id, cancellationToken);
+        if (waybill is null)
+            throw new EntityNotFoundException();
 
-        throw new NotImplementedException();
+        _mapper.Map(data, waybill);
+        var updatedWaybill = _context.Waybills.Update(waybill);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return _mapper.Map<WaybillDto>(updatedWaybill);
+    }
+
+    public async Task DeleteByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var waybill = await _context.Waybills.FirstOrDefaultAsync(s => s.Id == id, cancellationToken);
+        if (waybill is null)
+            throw new EntityNotFoundException();
+
+        _context.Remove(waybill);
+        await _context.SaveChangesAsync(cancellationToken);
     }
 }
